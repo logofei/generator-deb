6186b22992fc82c4348da407ebf9ada5c6c60ff5
shasa.xf <shasa.xf@taobao.com>
2014-05-19 18:27:53 +0800

    add

--- app/index.js
+++ app/index.js
@@ -3,8 +3,6 @@ var util = require('util');
 var path = require('path');
 var yeoman = require('yeoman-generator');
 var yosay = require('yosay');
-var chalk = require('chalk');
-
 
 var DebGenerator = yeoman.generators.Base.extend({
   init: function () {
